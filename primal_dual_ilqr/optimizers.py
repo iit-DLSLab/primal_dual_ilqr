11ea5637d3b275dce3b469dbd42d61c221451df2
lamatucci <lorenzo.amatucci@iit.it>
2024-12-11 17:51:46 +0100

    clean-up and moved files to mpx

--- primal_dual_ilqr/optimizers.py
+++ primal_dual_ilqr/optimizers.py
@@ -130,7 +130,9 @@ def compute_search_direction(
     R = R_pad[:-1]
     M = M_pad[:-1]
 
-    Q, R = regularize(Q, R, M, True, 1e-6)
+    # Q, R = regularize(Q, R, M, True, 1e-6)
+    Q = Q + 1e-3 * np.eye(Q.shape[-1])
+    R = R + 1e-3 * np.eye(R.shape[-1])
 
     linearizer = linearize(lagrangian(cost, dynamics, x0), argnums=5)
     q, r_pad = linearizer(X, pad(U), np.arange(T + 1), pad(V[1:]), V)
